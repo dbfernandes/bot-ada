73df70c52b9fd46f960a8aa2964cf3198386aebf
David Fernandes de Oliveira <dbfernandes@gmail.com>
2026-06-25 05:55:59 -0400

    Resolução de bug devido a falta de permissões do bot

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cron = require('node-cron');
 dotenv.config();
 const FORUM_CHANNEL_ID = process.env.FORUM_CHANNEL_ID;
 const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
+const CARGO_EQUIPE_ID = process.env.CARGO_EQUIPE_ID;
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
@@ -12,18 +13,24 @@ const TEMPLATE = `Data: {{DATA}}
 Membro da equipe:
 
 Tarefa Atual:
-Código do Jira: WF-251
+Códigos do Jira: SCRUM-251
 
-**1. O que foi feito ontem?**
+1. O que foi feito ontem?
 
-**2. O que será feito hoje?**
+- Status da Tarefa:
+- Porcentagem de conclusão:
+- Pull Request aberto: 
 
-**3. Há algum impedimento?**`;
+2. O que irei fazer hoje?
+
+3. Existe algum impedimento bloqueando meu trabalho?
+`;
 
 async function criarDaily() {
   const canal = await client.channels.fetch(FORUM_CHANNEL_ID);
-  if (canal.type !== ChannelType.GuildForum) {
-    console.error('O canal não é um fórum.');
+
+  if (canal.type !== ChannelType.GuildText) {
+    console.error('O canal não é um canal de texto.');
     return;
   }
 
@@ -31,19 +38,27 @@ async function criarDaily() {
     day: '2-digit', month: '2-digit', timeZone: 'America/Manaus',
   });
 
-  await canal.threads.create({
+  const thread = await canal.threads.create({
     name: `Daily ${data}`,
-    message: { content: TEMPLATE.replace('{{DATA}}', data) },
+    autoArchiveDuration: 1440,
+    type: ChannelType.PublicThread,
+    reason: 'Thread diária de daily scrum',
   });
 
-  console.log(`Post "Daily ${data}" criado.`);
+  await thread.send({
+    content: `<@&${CARGO_EQUIPE_ID}>\n\n${TEMPLATE.replace('{{DATA}}', data)}`,
+    allowedMentions: { roles: [CARGO_EQUIPE_ID] },
+  });
+
+  console.log(`Thread "Daily ${data}" criada.`);
 }
+
 
 client.once('clientReady', () => {
   console.log(`Logado como ${client.user.tag}`);
 
   // Seg a Sex às 05:00, horário de Manaus
-  cron.schedule('0 5 * * 1-5', criarDaily, {
+  cron.schedule('0 8 * * 1-5', criarDaily, {
     timezone: 'America/Manaus',
   });
 });
